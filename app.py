d7f817525f523b2e4a7da5fb92ebe1d54507f5ff
hadarby <hadarbey@gmail.com>
2020-06-06 18:49:02 +0300

    Merged new project

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask
 
+
 ###### App setup
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
 ###### Pages
-## Home
-from pages.home.home import home
-app.register_blueprint(home)
+## Homepage
+#TODO: we need to change homepage to homepage page
+from pages.homepage.homepage import homepage
+app.register_blueprint(homepage)
 
 ## About
 from pages.about.about import about
 app.register_blueprint(about)
+
+## Catalog
+from pages.catalog.catalog import catalog
+app.register_blueprint(catalog)
+
+## Page error handlers
+from pages.page_error_handlers.page_error_handlers import page_error_handlers
+app.register_blueprint(page_error_handlers)
 
 ## Categories
 from pages.categories.categories import categories
@@ -33,28 +43,10 @@ app.register_blueprint(catalogue)
 from pages.customer_feedback.customer_feedback import customer_feedback
 app.register_blueprint(customer_feedback)
 
-## customer_page
+##customer_page
 from pages.customer_page.customer_page import customer_page
 app.register_blueprint(customer_page)
-
-
-# ## Login
-# from pages.login.login import login
-# app.register_blueprint(login)
-#
-# ## Logout
-# from pages.logout.logout import logout
-# app.register_blueprint(logout)
-#
-# ## Page error handlers
-# from pages.page_error_handlers.page_error_handlers import page_error_handlers
-# app.register_blueprint(page_error_handlers)
-#
-#
 ###### Components
 ## Main menu
 from components.main_menu.main_menu import main_menu
 app.register_blueprint(main_menu)
-
-if __name__ == '__main__':
-    app.run()
