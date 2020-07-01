48e9813136a538e28720109437d685d7dc173a35
Gil <gilshw91@outlook.com>
2020-07-01 21:26:48 +0300

    small things

--- entities.py
+++ entities.py
@@ -20,6 +20,16 @@ class Customer:
         sql = 'SELECT * FROM customer WHERE email_address=%s AND password=%s'
         return dbManager.fetch(sql, (email, password))
 
+    def get_password(self, email):
+        sql = 'SELECT password FROM customer WHERE email_address = %s'
+        return dbManager.fetch(sql, (email, ))
+
+    def change_password(self, new_password, email):
+        sql = 'UPDATE customer SET password = %s WHERE email_address = %s'
+        dbManager.commit(sql, (new_password, email))
+        flash("Changed successfully")
+        return
+
     def add_customer(self, email_address, user, password, first_name, last_name, country, city, street,
                  number, zip, phone_number):
         """ Add new customer to data base"""
@@ -36,6 +46,29 @@ class Customer:
         flash('You were successfully Signed-up now you can Sign in')
         return
 
+    def get_address(self, email):
+        """ Returns the full address of a user, which identify by his email"""
+        sql = '''
+                SELECT c.email_address, c.country, c.city, c.street, c.number, z.zip 
+                FROM customer AS c 
+                JOIN zips AS z ON c.country=z.country 
+                AND c.city=z.city AND c.street=z.street AND c.number=z.number 
+                WHERE email_address=%s
+              '''
+        return dbManager.fetch(sql, (email, ))
+
+    def update_address(self, country, city, street, number, zip, email):
+        sql_1 = '''
+                    UPDATE customer SET country =%s, city = %s, street = %s, number = %s
+                    WHERE email_address = %s
+                '''
+        sql_2 = '''
+                    UPDATE zips SET  zip = %s
+                    WHERE country = %s AND city = %s AND street = %s AND number = %s
+                '''
+        dbManager.commit(sql_1, (country, city, street, number, email))
+        dbManager.commit(sql_2, (zip, country, city, street, number, email))
+        return
 
 class Category:
     def __init__(self):
@@ -55,3 +88,66 @@ class Product:
         """ returns a list of all products """
         sql = 'SELECT * FROM product'
         return dbManager.fetch(sql)
+
+class Review:
+    def __init__(self):
+        pass
+
+    def get_review_by_email(self, email):
+        """ This method returns the reviews on product
+        which was submitted by user (his email address). """
+        sql = ''' 
+                SELECT r.date, r.rank, r.content, r.email_address, p.name
+                FROM review AS r 
+                JOIN product AS p 
+                ON r.id = p.id 
+                WHERE email_address = %s
+              '''
+        return dbManager.fetch(sql, (email, ))
+
+
+class Credit:
+    def __init__(self):
+        pass
+
+    def get_credit_by_email(self, email):
+        """Return the credit data by the user email"""
+        sql = 'SELECT * FROM credit WHERE email_address = %s'
+        return dbManager.fetch(sql, (email,))
+
+    def update_credit(self, credit_number, exp, cvv, email):
+        """updates and changes a credit data by the users' email"""
+        sql = '''
+                UPDATE credit SET credit_card_number = %s, expiration_date = %s, cvv = %s
+                WHERE email_address = %s
+              '''
+        dbManager.commit(sql, (credit_number, exp, cvv, email))
+        return
+
+    def add_credit(self, credit_number, exp, cvv, email):
+        """if the user doesnt have a credit in the database
+        this method will insert the credit data by the users' email"""
+        sql = '''
+                INSERT INTO credit (credit_card_number, expiration_date, cvv, email_address)
+                VALUES (%s, %s, %s, %s)
+              '''
+        dbManager.commit(sql, (credit_number, exp, cvv, email))
+        return
+
+
+class Order:
+    def __init__(self):
+        pass
+
+    def get_history(self, email):
+        """This method returns the history of all the orders (their details) and the products
+            which the user submitted on."""
+        sql = '''
+                SELECT o.number, o.date_of_order, o.email_address, i.quantity, p.id,p.name, p.price, p.img
+                FROM 'order' AS o 
+                JOIN include AS i ON o.number = i.number 
+                JOIN product AS p ON i.sku = p.id
+                WHERE email_address = %s
+                # ORDER BY o.date_of_order DESC 
+              '''
+        return dbManager.fetch(sql, (email,))
--- pages/customer_page/customer_page.py
+++ pages/customer_page/customer_page.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, session, flash, redirect, url_for
 from utilities.db.db_manager import dbManager
+from entities import *
 
 
 # customer_page blueprint definition
@@ -9,27 +10,16 @@ customer_page = Blueprint('customer_page', __name__, static_folder='static', sta
 # Routes
 @customer_page.route('/customer_page')
 def index():
-    products = dbManager.fetch('SELECT * FROM product')
-    reviews = dbManager.fetch('''SELECT r.date, r.rank, r.content, r.email_address, p.name
-                                FROM review AS r 
-                                JOIN product AS p ON r.id=p.id 
-                                WHERE email_address=%s''', (session['email'],))
-    credit = dbManager.fetch('SELECT * FROM credit WHERE email_address=%s', (session['email'],))
-    histories = dbManager.fetch('''SELECT o.number, o.date_of_order, o.email_address, i.quantity, p.id,
-                                            p.name, p.price, p.img
-                                FROM `order` AS o 
-                                JOIN include AS i ON o.number=i.number 
-                                JOIN product AS p ON i.sku=p.id
-                                WHERE email_address=%s''', (session['email'],))
-    address = dbManager.fetch('''SELECT c.email_address, c.country, c.city, c.street, c.number, z.zip 
-                                FROM customer AS c 
-                                JOIN zips AS z ON c.country=z.country 
-                                AND c.city=z.city AND c.street=z.street AND c.number=z.number 
-                                WHERE email_address=%s''', (session['email'],))
+    email = session['email']
+    products = Product().get_all()
+    reviews = Review().get_review_by_email(email)
+    credit = Credit().get_credit_by_email(email)
+    histories = Order().get_history(email)
+    address = Customer().get_address(email)
     # product_name = dbManager.fetch('''
     # SELECT name from product
     # WHERE product.id = %s''', (reviews.id,))
-    user_data = dbManager.fetch('SELECT * FROM customer WHERE email_address = %s', (session['email'],))
+    user_data = Customer().get_user_by_email(email)
     return render_template('customer_page.html', products=products, user_data=user_data, reviews=reviews, credit=credit,
                            histories=histories, address=address)
 
@@ -38,60 +28,50 @@ def index():
 
 @customer_page.route('/update_address', methods=['POST'])
 def update_address():
-    city = request.form.get('city')
-    street = request.form.get('street')
-    number = request.form.get('number')
-    zip_code = request.form.get('zip_code')
-    dbManager.commit('UPDATE customer SET city = %s, street = %s, number = %s', (city, street, number))
-    dbManager.commit('UPDATE zips SET zip = %s', (zip_code,))
-
-    products = dbManager.fetch('SELECT * FROM product')
-    reviews = dbManager.fetch('''SELECT r.date, r.rank, r.content, r.email_address, p.name
-                                    FROM review AS r 
-                                    JOIN product AS p ON r.id=p.id 
-                                    WHERE email_address=%s''', (session['email'],))
-    credit = dbManager.fetch('SELECT * FROM credit WHERE email_address=%s', (session['email'],))
-    histories = dbManager.fetch('''SELECT o.number, o.date_of_order, o.email_address, i.quantity, p.id,
-                                                p.name, p.price, p.img
-                                    FROM `order` AS o 
-                                    JOIN include AS i ON o.number=i.number 
-                                    JOIN product AS p ON i.sku=p.id
-                                    WHERE email_address=%s''', (session['email'],))
-    address = dbManager.fetch('''SELECT c.email_address, c.country, c.city, c.street, c.number, z.zip 
-                                    FROM customer AS c 
-                                    JOIN zips AS z ON c.country=z.country 
-                                    AND c.city=z.city AND c.street=z.street AND c.number=z.number 
-                                    WHERE email_address=%s''', (session['email'],))
+    email = session['email']
+    country = request.form['country']
+    city = request.form['city']
+    street = request.form['street']
+    number = request.form['number']
+    zip_code = request.form['zip_code']
+    # Updates with address
+    Customer().update_address(country, city, street, number, zip_code, email)
+    products = Product().get_all()
+    reviews = Review().get_review_by_email(email)
+    credit = Credit().get_credit_by_email(email)
+    histories = Order().get_history(email)
+    address = Customer().get_address(email)
     # product_name = dbManager.fetch('''
     # SELECT name from product
     # WHERE product.id = %s''', (reviews.id,))
-    user_data = dbManager.fetch('SELECT * FROM customer WHERE email_address = %s', (session['email'],))
-
+    user_data = Customer().get_user_by_email(email)
     return redirect(url_for('customer_page.index', products=products, user_data=user_data, reviews=reviews, credit=credit,
                             histories=histories, address=address))
 
 
 @customer_page.route('/update_password', methods=['POST'])
 def update_password():
+    email = session['email']
     old_pass = request.form.get('pwd')
     # new_pass = request.form.get('npwd')
     re_new_pass = request.form.get('npwd2')
-    password = dbManager.fetch('SELECT password FROM customer WHERE email_address=%s', (session['email'],))
+    password = Customer().get_password(email)
     if password == old_pass:
-        dbManager.commit('UPDATE customer SET password = %s WHERE email_address = %s', (re_new_pass, session['email']))
-        flash("Changed successfully")
+        Customer().change_password(re_new_pass, email)
+        # flash("Changed successfully")
     else:
         flash("Wrong Password")
-
     return render_template('customer_page.html')
 
 
 @customer_page.route('/update_credit', methods=['POST'])
 def update_credit():
+    email = session['email']
     credit = request.form.get('credit')
     exp = request.form.get('exp')
     cvv = request.form.get('cvv')
-    dbManager.commit('''UPDATE credit SET credit_card_number = %s, expiration_date = %s, cvv = %s
-                        WHERE email_address = %s''', (credit, exp, cvv, session['email']))
+    has_credit = Credit().get_credit_by_email(email)
+    if has_credit:
+        Credit().update_credit(credit, exp, cvv, email)
 
-    return render_template('customer_page.html')
+    return redirect(url_for('customer_page.index'))
