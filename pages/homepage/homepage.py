747299c5ce15a8ff5c9aa88867b6cac8dd6dd4c0
Gil <gilshw91@outlook.com>
2020-07-06 01:10:17 +0300

    carousel

--- pages/homepage/homepage.py
+++ pages/homepage/homepage.py
@@ -9,8 +9,9 @@ homepage = Blueprint('homepage', __name__, static_folder='static', static_url_pa
 #  Routes
 @homepage.route('/')
 def index():
-    products_data = Product().get_all()
-    return render_template('homepage.html', products=products_data)
+    products_data = Product().get_all()  # The products that will appear in the carousel
+    product_week = Product().get_product(100000002)  # The product that will be pop-up as the product of the week
+    return render_template('homepage.html', products=products_data, product_week=product_week)
 
 
 @homepage.route('/homepage')
